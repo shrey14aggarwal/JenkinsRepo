b9c9c57790bb4cc29f268ce98b014bed9263ff78
shrey14aggarwal <47663477+shrey14aggarwal@users.noreply.github.com>
2020-07-21 11:54:55 +0200

    Add files via upload

---
+++ first_code/src/first_code/PracticeCode.java
@@ -0,0 +1,22 @@
+package first_code;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class PracticeCode {
+
+	
+	public static void main(String[] args)
+	{
+		 int x = 5;
+		 int y = 3;
+		 int z = x + y;
+		 
+		 System.out.println(z);		 
+		
+	}
+	
+	
+}
